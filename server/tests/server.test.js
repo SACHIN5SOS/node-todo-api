6cdc90f681f26ed622edc9229b33b2cc422bc719
SACHIN5SOS <sachinb443@gmail.com>
2018-01-11 13:45:15 +0530

    Testing od Get route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/Todo');
 
+const todos=[{
+    text: 'Making Lunch'
+},{
+    text:'Doing dishes'
+}];
 
 beforeEach((done)=>{
-    Todo.remove({}).then(()=> done());
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos);
+    }).then(()=>done());
 });
 
 describe('Post /todo',()=>{
@@ -24,7 +31,7 @@ describe('Post /todo',()=>{
                     return done(err);
                 }
 
-                Todo.find().then((todos)=>{
+                Todo.find({text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -43,9 +50,22 @@ describe('Post /todo',()=>{
                 }
 
                 Todo.find().then((todos)=>{
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e)=> done(e));
             });
     });
+
+    describe('Get /todo',()=>{
+        it('should get all todos',(done)=>{
+            request(app)
+                .get('/todo')
+                .expect(200)
+                .expect((res)=>{
+                    expect(res.body.todos.length).toBe(2);
+                })
+                .end(done);
+        })
+    })
+
 });
