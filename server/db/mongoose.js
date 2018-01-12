918419dfe54eb827ef0cb53a7c65b27cdb4e455b
SACHIN5SOS <sachinb443@gmail.com>
2018-01-13 02:26:13 +0530

    New username

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Sachin Bhandari:ilovemyfamily123@ds046357.mlab.com:46357/nodetodoapp');
+mongoose.connect('mongodb://sachin5sos:ilovemyfamily123@ds046357.mlab.com:46357/nodetodoapp');
 
 module.exports={mongoose}
