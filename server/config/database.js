760243effedbd876aaefef6e9607e1451eb9e5f6
SACHIN5SOS <sachinb443@gmail.com>
2018-01-13 03:35:37 +0530

    Change files

--- server/config/database.js
+++
@@ -1,7 +0,0 @@
-if(process.env.NODE_ENV==='production'){
-    module.exports={mongoURI:
-        'mongodb://sachin5sos:ilovemyfamily123@ds046357.mlab.com:46357/nodetodoapp'
-    }
-}else{
-    module.exports= {mongoURI: 'mongodb://localhost:27017'}
-}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
-
-const db= require('./../config/database');
 mongoose.Promise = global.Promise;
-mongoose.connect(db.mongoURI,{
-    useMongoClient:true
-}).then(()=>console.log('Mongo Connected'))
-.catch(err=>console.log(err));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose}
